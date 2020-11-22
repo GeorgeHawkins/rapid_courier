fb5a4700b117b0d0a138d03d994647838f0a7bd2
George <george.hawk88@gmail.com>
2020-11-22 22:48:54 +1000

    fix for server disconnects

--- Main.js
+++ Main.js
@@ -10,19 +10,52 @@ app.use(express.json());
 
 //Database
 
-const db = mysql.createConnection({
+var dbConfig = {
   host: 'us-cdbr-east-02.cleardb.com',
   user: 'bccf3b9f4740a9',
   password: 'f56ade09',
   database: 'heroku_bdbaf8607a93536'
-});
+}
 
-db.connect(function(err) {
-  if (err) {
-    console.log('database error');
-    throw err;
-  }
-});
+var db;
+
+function handleDisconnect() {
+  db = mysql.createConnection(dbConfig);
+  db.connect( function onConnect(err) {
+    if (err) {
+      console.log('Error when connecting to the ClearDB database: ' + err);
+      setTimeout(handleDisconnect, 10000);
+    }
+  });
+
+  db.on('error', function onError(err) {
+    console.log('db error', err);
+    if (err.code == 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    }
+    else {
+      throw err;
+    }
+  });
+
+
+}
+
+handleDisconnect();
+
+// const db = mysql.createConnection({
+//   host: 'us-cdbr-east-02.cleardb.com',
+//   user: 'bccf3b9f4740a9',
+//   password: 'f56ade09',
+//   database: 'heroku_bdbaf8607a93536'
+// });
+
+// db.connect(function(err) {
+//   if (err) {
+//     console.log('database error');
+//     throw err;
+//   }
+// });
 
 
 new Router(app, db);
