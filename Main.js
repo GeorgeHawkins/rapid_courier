5d3ca509dcbe8707172ec33b11d3f42cb7a65dee
George <george.hawk88@gmail.com>
2020-11-22 21:55:52 +1000

    fix port for heroku

--- Main.js
+++ Main.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const mysql = require('mysql');
 const Router = require('./Router');
+const { Console } = require('console');
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
@@ -31,4 +32,8 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server is up and listening on: ' + PORT)
+});
