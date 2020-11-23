97e947634948084527c1590874585d3c7d10b06f
George <george.hawk88@gmail.com>
2020-11-23 15:56:15 +1000

    some cleanup + basic html changes

--- Main.js
+++ Main.js
@@ -8,61 +8,6 @@ const { Console } = require('console');
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 
-//Database
-
-// var dbConfig = {
-//   connectionLimit: 10,
-//   host: 'us-cdbr-east-02.cleardb.com',
-//   user: 'bccf3b9f4740a9',
-//   password: 'f56ade09',
-//   database: 'heroku_bdbaf8607a93536'
-// }
-
-// var db;
-// const pool = mysql.createPool(dbConfig);
-
-// function getConnection() {
-//   return pool;
-// }
-
-// function handleDisconnect() {
-//   db = mysql.createConnection(dbConfig);
-//   db.connect( function onConnect(err) {
-//     if (err) {
-//       console.log('Error when connecting to the ClearDB database: ' + err);
-//       setTimeout(handleDisconnect, 10000);
-//     }
-//   });
-
-//   db.on('error', function onError(err) {
-//     console.log('db error', err);
-//     if (err.code == 'PROTOCOL_CONNECTION_LOST') {
-//       handleDisconnect();
-//     }
-//     else {
-//       throw err;
-//     }
-//   });
-
-
-// }
-
-// handleDisconnect();
-
-// const db = mysql.createConnection({
-//   host: 'us-cdbr-east-02.cleardb.com',
-//   user: 'bccf3b9f4740a9',
-//   password: 'f56ade09',
-//   database: 'heroku_bdbaf8607a93536'
-// });
-
-// db.connect(function(err) {
-//   if (err) {
-//     console.log('database error');
-//     throw err;
-//   }
-// });
-
 var db;
 
 new Router(app, db);
